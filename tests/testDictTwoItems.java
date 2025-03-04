54449469b127598e73129c6555af93f7ec75c4f1
ToleteMaster <153006817+ToleteMaster@users.noreply.github.com>
2025-03-04 12:31:34 +0000

    Añadidos test de len, añadir y elminar elementos en testDictTwoItems

--- tests/testDictTwoItems.java
+++ tests/testDictTwoItems.java
@@ -1,5 +1,47 @@
 package dictjava.tests;
 
+import org.junit.*;
+import org.junit.runner.JUnitCore;
+import static org.junit.Assert.*;
+import source.dict;
+
 public class testDictTwoItems {
-    
+
+    private dict dict;
+
+    @Before
+    public void init() {
+        dict = new dict();
+        dict.add();
+        dict.add();
+    }
+
+    /**
+     * test de len con un diccionario de 2 elementos
+     */
+    @Test
+    public void testLen() {
+        int dictLen = dict.len();
+        assertEquals("El tamaño debe de ser 2 después de inicializar",2,dictLen);
+    }
+
+    /**
+     * test de añadir un elemento al diccionario con 2 elementos
+     */
+    @Test
+    public void testAdd1() {
+        dict.add();
+        int dictLen = dict.len();
+        assertEquals("El tamaño debe de ser 3",3,dictLen);
+    }
+
+    /**
+     * test eliminar un elemento del diccionario
+     */
+    @Test
+    public void testRemove() {
+        dict.remove();
+        int dictLen = dict.len();
+        assertEquals("El tamaño debe de ser 1 al eliminar",1,dictLen);
+    }
 }
