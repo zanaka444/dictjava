0d37b2fc8c4f727c4e8d431e577c10f25e35dd43
ToleteMaster <153006817+ToleteMaster@users.noreply.github.com>
2025-03-04 13:08:23 +0000

    comentarios de los test a realizar en testDictManyItems

--- tests/testDictManyItems.java
+++ tests/testDictManyItems.java
@@ -1,5 +1,37 @@
 package dictjava.tests;
 
+
+import dictjava.source.dict;
+import org.junit.*;
+import org.junit.runner.JUnitCore;
+import static org.junit.Assert.*;
+import source.dict;
+
+
 public class testDictManyItems {
-    
+
+    private dict dict;
+
+    @Before
+    public void init() {
+        dict = new dict();
+    }
+
+    /**
+     * test de len con un diccionario con varios elementos
+     */
+
+
+    /**
+     * test de añadir un elemento al diccionario con varios elementos
+     */
+
+    /**
+     * test de eliminar un elemento al diccionario con varios elementos
+     */
+
+    /**
+     *
+     */
+
 }
